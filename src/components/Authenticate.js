eba1280976f787c297cc7fe1fbb2d81d0aa0f50c
Lydia Gregory <ljg2gb@virginia.edu>
2020-06-15 23:34:28 -0600

    adds toggle button for Login form

--- src/components/Authenticate.js
+++ src/components/Authenticate.js
@@ -1,12 +1,24 @@
-import React from 'react';
+import React, { Component } from 'react';
 import LoginForm from './LoginForm';
 import SignupForm from './SignupForm';
 
- export default function Authenticate() {
-     return(
-         <div>
-            <LoginForm />
-            <SignupForm />
-         </div>
-     )
- }
+ export default class Authenticate extends Component {
+    state = {
+        isLogin: true
+    } 
+
+    toggleForms = () => {
+        this.setState({ isLogin: !this.state.isLogin})
+    }
+
+     render() {
+        return(
+            <div>
+                {this.state.isLogin ? <LoginForm /> : <SignupForm /> }
+                <button onClick={this.toggleForms}>
+                    {this.state.isLogin ? "Need to Signup?" : "Need to Login?"}
+                </button>
+            </div>
+        )
+     }
+ } 
