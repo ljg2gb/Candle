ec7ddfae48874ec4739561a6998937c372d42e7a
Lydia Gregory <ljg2gb@virginia.edu>
2020-06-22 13:32:34 -0600

    refactors Sass files

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -4,7 +4,7 @@ import { Link } from 'react-router-dom';
 export default function Navbar() {
   return (
     <div className="navbar">
-      <p>Candle</p>
+      <p className="app-title">Candle</p>
       {/* <Link className='link' to='/'>Home </Link> */}
       <Link className='link' to='/login'>Sign out </Link>
     </div>
